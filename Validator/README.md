17a1bfdbefa8a98d8464df45529f0add3104b6c9
deepshikha2020 <deepshikha607497@gmail.com>
2022-01-17 16:45:56 +0530

    Validator: Function to get varibale types

--- Validator/README.md
+++ Validator/README.md
@@ -1,3 +1,3 @@
 # Validator
 
-Coming Soon
+1. [Create a function to get variable type.](https://github.com/deepshikha2020/JavaScript/blob/main/Validator/getVarType.js)
---
+++ Validator/getVarType.js
@@ -0,0 +1,13 @@
+const getVarType = (data) => {
+  const varType = typeof data;
+  if (varType === "object") {
+    if (data === null) return "null";
+    if (Array.isArray(data)) return "array";
+    if (data instanceof Set) return "set";
+    if (data instanceof Map) return "map";
+  }
+  return varType;
+};
+
+const result = getVarType({});
+console.log(result);
