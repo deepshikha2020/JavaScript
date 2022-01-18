8caa156ad8f25c1ae804f19b4c84cadca0fdd8f9
deepshikha2020 <deepshikha607497@gmail.com>
2022-01-18 19:11:13 +0530

    Array: To remove Undefined and Null variables from the nested array.

--- Array/removeUndefinedAndNull.js
+++ Array/removeUndefinedAndNull.js
@@ -1,8 +1,15 @@
-const removeUndfinedAndNull =(data) => {
-    return data.filter(( element ) => {
+/**
+ * To remove Undefined and Null variables from the given array.
+ * @param {array} data 
+ * @returns array
+ */
+const removeUndefinedAndNull =(data) => {
+    return data.map((element) => {
+        return Array.isArray(element) ? removeUndefinedAndNull(element) : element;
+    }).filter(( element) => {
         return element !== undefined && element !== null;
     });
 }
 
-const result = removeUndfinedAndNull([42, null, 21, undefined, 50, 40, undefined, 9, null]);
+const result = removeUndefinedAndNull([42,[null, 1, 2], null, 21, [undefined,[1,3,null], null], 50, 40, undefined, 9, null]);
 console.log(result);
